a135a783caa9f6dd6c9c4033d0cbcd52de06ca6a
kimyuchan <rladbcks103@naver.com>
2025-07-07 20:20:17 +0900

    initial setting

---
+++ src/main/java/com/coredisc/CorediscApplication.java
@@ -0,0 +1,13 @@
+package com.coredisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorediscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorediscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coredisc/CorediscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coredisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorediscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
