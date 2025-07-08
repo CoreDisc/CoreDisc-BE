bc0db12536329759e2443dd098ce2ad9909d4960
kimyuchan <rladbcks103@naver.com>
2025-07-08 18:16:20 +0900

    feat: answer, post, publicity enums 생성

---
+++ src/main/java/com/coredisc/domain/common/enums/AnswerType.java
@@ -0,0 +1,5 @@
+package com.coredisc.domain.common.enums;
+
+public enum AnswerType {
+    IMAGE,TEXT
+}
---
+++ src/main/java/com/coredisc/domain/common/enums/PublicityType.java
@@ -0,0 +1,5 @@
+package com.coredisc.domain.common.enums;
+
+public enum PublicityType {
+    OFFICIAL,PERSONAL,CIRCLE
+}
