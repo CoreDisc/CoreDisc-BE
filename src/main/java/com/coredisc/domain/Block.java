68cbdce935ee2ad6f0e01f35ac4a3eb71739b9c3
hmj6589 <hmj6589@gmail.com>
2025-07-10 00:34:34 +0900

    feat: block 엔티티 생성

---
+++ src/main/java/com/coredisc/domain/Block.java
@@ -0,0 +1,28 @@
+package com.coredisc.domain;
+
+import com.coredisc.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Block extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 차단 요청한 주체 멤버
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocker_id", nullable = false)
+    private Member blocker;
+
+    // 차단 대상 멤버
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocked_id", nullable = false)
+    private Member blocked;
+
+}
