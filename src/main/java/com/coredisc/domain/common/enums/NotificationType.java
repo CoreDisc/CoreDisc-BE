1cbeed6a14ab979497075855ea64162b740f3e97
hmj6589 <hmj6589@gmail.com>
2025-07-10 18:33:48 +0900

    feat: notification 관련 엔티티 작성

---
+++ src/main/java/com/coredisc/domain/common/enums/NotificationType.java
@@ -0,0 +1,9 @@
+package com.coredisc.domain.common.enums;
+
+public enum NotificationType {
+    DAILY_REMINDER, //질문 답변 해줘 리마인더
+    UNANSWERED_QUESTION, //질문 답변 마무리해줘 리마인더
+    MONTHLY_REPORT, //한 달 리포트
+    FOLLOW, //팔로우 알림 (누가 날 팔로우 했어요)
+    SHARED_SAVED //공유 질문 저장 알림 (내가 작성한 공유 질문 누가 저장했어요)
+}
