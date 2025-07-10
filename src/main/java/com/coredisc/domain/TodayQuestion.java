b530a23224ef6c8325f6f5e269ef799ae9caa0c5
dokyung-kang <kangdk025@gmail.com>
2025-07-10 18:22:10 +0900

    Feat: 선택한 질문 엔티티 설정

--- src/main/java/com/coredisc/domain/TodayQuestion.java
+++ src/main/java/com/coredisc/domain/TodayQuestion.java
@@ -1,10 +1,12 @@
 package com.coredisc.domain;
 
 import com.coredisc.domain.common.BaseEntity;
+import com.coredisc.domain.common.enums.QuestionType;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -17,9 +19,22 @@ public class TodayQuestion extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    protected LocalDateTime selectedDate;
 
-    // TODO: 연관관계 설정, 엔티티 정의하고 todo 제거해주세요.
-    // - @ManyToOne Member
-    // - @ManyToOne PersonalQuestion
-    // - @ManyToOne OfficialQuestion
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private QuestionType questionType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "official_question_id")
+    private OfficialQuestion officialQuestion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "personal_question_id")
+    private PersonalQuestion personalQuestion;
 }
