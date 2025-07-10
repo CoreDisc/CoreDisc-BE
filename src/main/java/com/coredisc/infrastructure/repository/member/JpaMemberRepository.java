c7f46e0f50845268d06663501d2c12fa905181b8
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-10 11:55:52 +0900

    Feat: JpaMemberRepository 작성

--- src/main/java/com/coredisc/infrastructure/repository/member/JpaMemberRepository.java
+++ src/main/java/com/coredisc/infrastructure/repository/member/JpaMemberRepository.java
@@ -3,5 +3,9 @@ package com.coredisc.infrastructure.repository.member;
 import com.coredisc.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaMemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
 }
