212287cd1bc75d6a349f49774ced8af3463b668e
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-11 00:08:03 +0900

    Feat: 스웨거 설정 파일에 JWT 설정 추가

--- src/main/java/com/coredisc/config/SwaggerConfig.java
+++ src/main/java/com/coredisc/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.coredisc.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,8 +19,22 @@ public class SwaggerConfig {
                 .version("v1.0") //버전
                 .title("CoreDisc API") //이름
                 .description("4개의 질문에 답변하는 SNS 서비스 API"); //설명
+
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(info);
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
 }
