1f61adb4c95ceb67abe0afa5a4bc49e7ba928d7b
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-12 06:17:16 +0900

    Feat: Terms, TermsType 작성

--- src/main/java/com/coredisc/domain/terms/Terms.java
+++ src/main/java/com/coredisc/domain/terms/Terms.java
@@ -1,26 +1,38 @@
-package com.coredisc.domain;
+package com.coredisc.domain.terms;
 
 import com.coredisc.domain.common.BaseEntity;
+import com.coredisc.domain.common.enums.TermsType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.*;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
-@Table(name = "terms")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Table(name = "terms")
+@Check(constraints = "version >= 100")
 public class Terms extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(100)")
+    private TermsType type;
+
+    @Lob
+    @Column(nullable = false, length = 256)
+    private String content;
+
+    @Column(nullable = false) // 100 == 1, 201 == 2.1
+    @Min(100)
     private Integer version;
 
+    @ColumnDefault("1")
     @Column(nullable = false)
-    private String title;
-
-    // TODO: 연관관계 설정, 엔티티 정의하고 todo 제거해주세요.
-
+    private Boolean isRequired;
 }
