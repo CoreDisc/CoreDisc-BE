3662484785f1fe77f84a39ab467f943398a43b63
jieun13 <ynb9598@gmail.com>
2025-07-11 23:07:27 +0900

    Feat: enum 추가

---
+++ src/main/java/com/coredisc/domain/common/enums/TimeZoneType.java
@@ -0,0 +1,6 @@
+package com.coredisc.domain.common.enums;
+
+public enum TimeZoneType {
+    DAWN, MORNING, DAY, AFTERNOON, EVENING, NIGHT;
+    //새벽(0-5시), 아침(6시-10시), 낮(11-13시), 오후(14-17시), 저녁(18-23시), 밤
+}
