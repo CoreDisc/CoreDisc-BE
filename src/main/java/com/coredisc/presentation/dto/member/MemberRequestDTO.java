b45461b46561baaa05087c61227dd9b918830eed
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-12 00:56:59 +0900

    Feat: 비밀번호 변경을 위한 dto 작성

---
+++ src/main/java/com/coredisc/presentation/dto/member/MemberRequestDTO.java
@@ -0,0 +1,29 @@
+package com.coredisc.presentation.dto.member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+public class MemberRequestDTO {
+
+    @Getter
+    public static class ResetPasswordDTO {
+
+        @NotBlank(message = "아이디 입력은 필수 입니다.")
+        @Schema(example = "my_coredisc")
+        String username;
+
+        @NotBlank(message = "변경할 비밀번호 입력은 필수입니다.")
+        @Pattern(
+                regexp = "^(?!.*(\\d)\\1{2})(?=(.*[A-Za-z]){1})(?=(.*\\d){1})(?!.*\\s).{10,}$|^(?!.*(\\d)\\1{2})(?=(.*[A-Za-z]){1})(?=(.*[^A-Za-z0-9]){1})(?!.*\\s).{10,}$|^(?!.*(\\d)\\1{2})(?=(.*\\d){1})(?=(.*[^A-Za-z0-9]){1})(?!.*\\s).{10,}$",
+                message = "비밀번호는 영문, 숫자, 특수문자 중 2종류 이상을 조합하여 10자리 이상이어야 하며, 동일한 숫자 3개 이상을 연속해서 사용할 수 없습니다."
+        )
+        @Schema(example = "coredisc123")
+        private String newPassword;
+
+        @NotBlank(message = "비밀번호 재확인은 필수입니다.")
+        @Schema(example = "coredisc123")
+        private String passwordCheck;
+    }
+}
