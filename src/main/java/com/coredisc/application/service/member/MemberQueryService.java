514c70efd40f07767824bc1cf1f592a22c2c1cfc
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-13 04:17:31 +0900

    Feat: 마이홈 사용자정보 조회 관련 service 작성

--- src/main/java/com/coredisc/application/service/member/MemberQueryService.java
+++ src/main/java/com/coredisc/application/service/member/MemberQueryService.java
@@ -2,8 +2,13 @@ package com.coredisc.application.service.member;
 
 
 import com.coredisc.domain.member.Member;
+import com.coredisc.presentation.dto.member.MemberResponseDTO;
 
 public interface MemberQueryService {
+
     // CurrentMemberResolver를 위해 Member 찾기
     Member getMemberByUsername(String username);
+
+    // 마이홈(본인) 사용자 정보 확인
+    MemberResponseDTO.MyHomeUserInfoDTO getMyHomeUserInfo(Member member);
 }
--- src/main/java/com/coredisc/application/service/member/MemberQueryServiceImpl.java
+++ src/main/java/com/coredisc/application/service/member/MemberQueryServiceImpl.java
@@ -1,9 +1,14 @@
 package com.coredisc.application.service.member;
 
 import com.coredisc.common.apiPayload.status.ErrorStatus;
+import com.coredisc.common.converter.MemberConverter;
 import com.coredisc.common.exception.handler.AuthHandler;
+import com.coredisc.domain.follow.FollowRepository;
 import com.coredisc.domain.member.Member;
-import com.coredisc.infrastructure.repository.member.JpaMemberRepository;
+import com.coredisc.domain.member.MemberRepository;
+import com.coredisc.domain.profileImg.ProfileImg;
+import com.coredisc.domain.profileImg.ProfileImgRepository;
+import com.coredisc.presentation.dto.member.MemberResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +16,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberQueryServiceImpl implements MemberQueryService {
 
-    private final JpaMemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final FollowRepository followRepository;
+    private final ProfileImgRepository profileImgRepository;
+
+
     @Override
     public Member getMemberByUsername(String username) {
         return memberRepository.findByUsername(username)
                 .orElseThrow(() -> new AuthHandler(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+    @Override
+    public MemberResponseDTO.MyHomeUserInfoDTO getMyHomeUserInfo(Member member) {
+
+        // 사용자의 팔로워 수
+        Long followerCount = followRepository.countByFollowingId(member.getId());
+
+        // 사용자의 팔로잉 수
+        Long followingCount = followRepository.countByFollowerId(member.getId());
+
+        // TODO: 총 디스크(월별 리포트 수)
+
+        // 사용자의 프로필 이미지
+        ProfileImg profileImg = profileImgRepository.findByMember(member);
+
+        return MemberConverter.toMyHomeUserInfoDTO(member, followerCount, followingCount, profileImg);
     }
 }
