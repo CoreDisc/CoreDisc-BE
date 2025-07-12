c1baf38c0b8a756748af256adf8a5e470f913279
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-13 04:56:42 +0900

    Feat: 회원가입 기본 프로필 설정 converter 작성

--- src/main/java/com/coredisc/common/converter/ProfileImgConverter.java
+++ src/main/java/com/coredisc/common/converter/ProfileImgConverter.java
@@ -1,5 +1,6 @@
 package com.coredisc.common.converter;
 
+import com.coredisc.domain.member.Member;
 import com.coredisc.domain.profileImg.ProfileImg;
 import com.coredisc.presentation.dto.profileImg.ProfileImgResponseDTO;
 
@@ -10,6 +11,14 @@ public class ProfileImgConverter {
         return ProfileImgResponseDTO.ProfileImgDTO.builder()
                 .profileImgId(profileImg.getId())
                 .imageUrl(profileImg.getImgUrl())
+                .build();
+    }
+
+    public static ProfileImg toProfileImg(Member member, ProfileImg defaultProfileImg) {
+
+        return ProfileImg.builder()
+                .imgUrl(defaultProfileImg.getImgUrl())
+                .member(member)
                 .build();
     }
 }
