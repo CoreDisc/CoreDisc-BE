5321c71d8f78149631e352aaf925eee67db3f0b9
jieun13 <ynb9598@gmail.com>
2025-07-14 12:39:19 +0900

    refactor: 리포트 통계 API에서 memberId 파라미터 제거 및 @CurrentMember 주입 방식 적용

--- src/main/java/com/coredisc/presentation/controllerdocs/ReportStatControllerDocs.java
+++ src/main/java/com/coredisc/presentation/controllerdocs/ReportStatControllerDocs.java
@@ -1,8 +1,11 @@
 package com.coredisc.presentation.controllerdocs;
 
 import com.coredisc.common.apiPayload.ApiResponse;
+import com.coredisc.domain.member.Member;
 import com.coredisc.presentation.dto.report.ReportResponseDTO;
+import com.coredisc.security.jwt.annotaion.CurrentMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,34 +19,34 @@ public interface ReportStatControllerDocs {
     ApiResponse<ReportResponseDTO.QuestionListDTO> getMonthlyQuestionList(
             @RequestParam("year") int year,
             @RequestParam("month") int month,
-            @RequestParam("memberId") Long memberId
+            @Parameter(hidden = true) @CurrentMember Member member
     );
 
     @Operation(summary = "기간별 최다 선택된 랜덤 질문 3순위 조회", description = "사용자가 특정 달 동안 가장 많이 선택한 랜덤 질문 3개를 조회합니다.")
     ApiResponse<ReportResponseDTO.MostSelectedQuestionDTO> getMostSelectedQuestions(
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            @RequestParam("memberId") Long memberId
+            @Parameter(hidden = true) @CurrentMember Member member
     );
 
     @Operation(summary = "기간별 응답 시간대 횟수 조회", description = "사용자의 특정 월(startDate~endDate)에 가장 많이 답변한 시간대와 시간대별 응답 수 리스트를 조회합니다.")
     ApiResponse<ReportResponseDTO.PeakHourDTO> getPeakHour(
             @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            @RequestParam("memberId") Long memberId
+            @Parameter(hidden = true) @CurrentMember Member member
     );
 
     @Operation(summary = "기간별 게시글의 daily_ 항목 최다 답변 조회", description = "사용자가 선택형 일기에서 특정 달에 가장 많이 선택한 옵션을 조회합니다.")
     ApiResponse<ReportResponseDTO.TopDailySelectionDTO> getMostSelectedDaily(
             @RequestParam("year") int year,
             @RequestParam("month") int month,
-            @RequestParam("memberId") Long memberId
+            @Parameter(hidden = true) @CurrentMember Member member
     );
 
     @Operation(summary = "사용자가 특정 달에 작성한 일기 내용 전체 출력", description = "특정 기간 동안 일기 게시글의 daily_ 항목 중 가장 많이 선택된 옵션을 조회합니다.")
     ApiResponse<ReportResponseDTO.DailyDetailListDTO> getDailyDetail(
             @RequestParam("year") int year,
             @RequestParam("month") int month,
-            @RequestParam("memberId") Long memberId
+            @Parameter(hidden = true) @CurrentMember Member member
     );
 }
