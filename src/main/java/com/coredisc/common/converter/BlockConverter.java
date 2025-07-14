7c8b4247faadd68c3dbce67b692af2cab490c11a
hmj6589 <hmj6589@gmail.com>
2025-07-15 02:58:59 +0900

    Refactor: BlockConverter static 메서드로 수정

--- src/main/java/com/coredisc/common/converter/BlockConverter.java
+++ src/main/java/com/coredisc/common/converter/BlockConverter.java
@@ -1,19 +1,19 @@
 package com.coredisc.common.converter;
 
-import com.coredisc.domain.Block;
+import com.coredisc.domain.block.Block;
 import com.coredisc.domain.member.Member;
 import com.coredisc.presentation.dto.block.BlockResponseDTO;
 
 public class BlockConverter {
 
-    public Block toBlock(Member member, Member blockedMember) {
+    public static Block toBlock(Member member, Member blockedMember) {
         return Block.builder()
                 .blocker(member)
                 .blocked(blockedMember)
                 .build();
     }
 
-    public BlockResponseDTO.BlockResultDTO toBlockResultDTO(Block block) {
+    public static BlockResponseDTO.BlockResultDTO toBlockResultDTO(Block block) {
         return  BlockResponseDTO.BlockResultDTO.builder()
                 .id(block.getId())
                 .blockerId(block.getBlocker().getId())
