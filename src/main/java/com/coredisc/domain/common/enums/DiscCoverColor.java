3e5fbad1cec8f1cf691d7d00da15115807adef67
jieun13 <ynb9598@gmail.com>
2025-07-16 01:15:49 +0900

    refactor: 오타 수정

--- src/main/java/com/coredisc/domain/common/enums/DiscCoverColor.java
+++ src/main/java/com/coredisc/domain/common/enums/DiscCoverColor.java
@@ -2,5 +2,5 @@ package com.coredisc.domain.common.enums;
 
 public enum DiscCoverColor {
     WHITE, // 기본 색상
-    GREEN, YELLOW, BLUE, PURPLE, PINK, LAVENDER, MINT, ORANGE;
+    GREEN, YELLOW, BLUE, PURPLE, PINK, LAVENDER, MINT, ORANGE
 }
