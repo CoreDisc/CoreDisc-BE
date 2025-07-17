c94ee903f01092159d91be4862255c5f0500a17c
hmj6589 <hmj6589@gmail.com>
2025-07-18 03:44:09 +0900

    Feat: 차단한 유저 목록 조회 Converter 작성

--- src/main/java/com/coredisc/common/converter/BlockConverter.java
+++ src/main/java/com/coredisc/common/converter/BlockConverter.java
@@ -4,6 +4,9 @@ import com.coredisc.domain.block.Block;
 import com.coredisc.domain.member.Member;
 import com.coredisc.presentation.dto.block.BlockResponseDTO;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BlockConverter {
 
     public static Block toBlock(Member member, Member blockedMember) {
@@ -19,6 +22,25 @@ public class BlockConverter {
                 .blockerId(block.getBlocker().getId())
                 .blockedId(block.getBlocked().getId())
                 .createdAt(block.getCreatedAt())
+                .build();
+    }
+
+    public static BlockResponseDTO.BlockedDTO toBlockedDTO(Block block) {
+        return BlockResponseDTO.BlockedDTO.builder()
+                .blockedId(block.getBlocked().getId())
+                .blockedNickname(block.getBlocked().getNickname())
+                .blockedUsername(block.getBlocked().getUsername())
+                .build();
+    }
+
+    public static BlockResponseDTO.BlockedListViewDTO toBlockedListViewDTO(List<Block> blockeds) {
+        List<BlockResponseDTO.BlockedDTO> dtos = blockeds.stream()
+                .map(BlockConverter::toBlockedDTO)
+                .collect(Collectors.toList());
+
+        return BlockResponseDTO.BlockedListViewDTO.builder()
+                .totalBlockedCount(blockeds.size())
+                .blockedList(dtos)
                 .build();
     }
 }
