a8266f18217e53328ded8168de95a90e153be466
hmj6589 <hmj6589@gmail.com>
2025-07-18 04:21:13 +0900

    Refactor: 차단 시 팔로우 관계라면 언팔 하도록 변경

--- src/main/java/com/coredisc/application/service/block/BlockCommandServiceImpl.java
+++ src/main/java/com/coredisc/application/service/block/BlockCommandServiceImpl.java
@@ -6,6 +6,8 @@ import com.coredisc.common.exception.handler.BlockHandler;
 import com.coredisc.common.exception.handler.MemberHandler;
 import com.coredisc.domain.block.Block;
 import com.coredisc.domain.block.BlockRepository;
+import com.coredisc.domain.follow.Follow;
+import com.coredisc.domain.follow.FollowRepository;
 import com.coredisc.domain.member.Member;
 import com.coredisc.domain.member.MemberRepository;
 import jakarta.transaction.Transactional;
@@ -19,6 +21,7 @@ public class BlockCommandServiceImpl implements BlockCommandService {
 
     private final MemberRepository memberRepository;
     private final BlockRepository blockRepository;
+    private final FollowRepository followRepository;
 
     @Override
     public Block block(Member member, Long targetId) {
@@ -33,6 +36,12 @@ public class BlockCommandServiceImpl implements BlockCommandService {
         // 이미 차단한 이력이 있을 경우
         if (blockRepository.existsByBlockerAndBlocked(member, target)) {
             throw new BlockHandler(ErrorStatus.ALREADY_BLOCKING);
+        }
+
+        // 팔로우 관계였다면 팔로우 취소
+        Follow follow = followRepository.findByFollowerAndFollowing(member, target);
+        if (follow != null) {
+            followRepository.delete(follow);
         }
 
         Block block = BlockConverter.toBlock(member, target);
