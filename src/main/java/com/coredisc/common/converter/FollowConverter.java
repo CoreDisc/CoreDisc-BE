adbfcff5b57450710c88f16a066374d80cf3bebd
hmj6589 <hmj6589@gmail.com>
2025-07-22 01:47:28 +0900

    Refactor: 친한 친구 조회 시 팔로워 DTO와 함께 쓰도록 수정

--- src/main/java/com/coredisc/common/converter/FollowConverter.java
+++ src/main/java/com/coredisc/common/converter/FollowConverter.java
@@ -19,15 +19,18 @@ public class FollowConverter {
                 .build();
     }
 
-    //TODO: profileImageUrl 추후 추가
+    // 팔로워, 친한 친구
     public static FollowResponseDTO.FollowerDTO toFollowerDTO(Follow follow) {
         return FollowResponseDTO.FollowerDTO.builder()
                 .followerId(follow.getFollower().getId())
-                .followerNickname(follow.getFollower().getNickname())
-                .followerUsername(follow.getFollower().getUsername())
+                .nickname(follow.getFollower().getNickname())
+                .username(follow.getFollower().getUsername())
+                //.profileImgDTO(ProfileImgConverter.toProfileImgDTO(follow.getFollower().getProfileImg()))
+                .isCircle(follow.isCircle())
                 .build();
     }
 
+    // TODO: 팔로워 목록 조회 - 수정 예정
     public static FollowResponseDTO.FollowerListViewDTO toFollowerListViewDTO(List<Follow> followers) {
         List<FollowResponseDTO.FollowerDTO> dtos = followers.stream()
                 .map(FollowConverter::toFollowerDTO)
@@ -55,7 +58,7 @@ public class FollowConverter {
                 .followingUsername(follow.getFollowing().getUsername())
                 .build();
     }
-
+    // TODO: 팔로잉 목록 조회 - 수정 예정
     public static FollowResponseDTO.FollowingListViewDTO toFollowingListViewDTO(List<Follow> followings) {
         List<FollowResponseDTO.FollowingDTO> dtos = followings.stream()
                 .map(FollowConverter::toFollowingDTO)
