078341891dcf7971d1bea34632961f5bcffe2489
hmj6589 <hmj6589@gmail.com>
2025-07-22 01:50:43 +0900

    Feat: 친한 친구 목록 조회 controller 작성

--- src/main/java/com/coredisc/presentation/controller/CircleController.java
+++ src/main/java/com/coredisc/presentation/controller/CircleController.java
@@ -1,11 +1,15 @@
 package com.coredisc.presentation.controller;
 
 import com.coredisc.application.service.follow.FollowCommandService;
+import com.coredisc.application.service.follow.FollowQueryService;
 import com.coredisc.common.apiPayload.ApiResponse;
 import com.coredisc.domain.member.Member;
 import com.coredisc.presentation.controllerdocs.CircleControllerDocs;
+import com.coredisc.presentation.dto.follow.FollowResponseDTO;
 import com.coredisc.security.jwt.annotaion.CurrentMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class CircleController implements CircleControllerDocs {
 
     private final FollowCommandService followCommandService;
+    private final FollowQueryService followQueryService;
 
     @PatchMapping("/api/circle/{targetId}")
     public ApiResponse<String> addToCircle(@CurrentMember Member member,
@@ -30,5 +35,15 @@ public class CircleController implements CircleControllerDocs {
         followCommandService.updateCircleStatus(member, targetId, false);
 
         return ApiResponse.onSuccess("성공적으로 친한 친구가 취소되었습니다.");
+    }
+
+    @GetMapping("/api/circles")
+    public ApiResponse<FollowResponseDTO.FollowerListDTO> getCircleFollowers(
+            @CurrentMember Member member,
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(required = false, defaultValue = "10") Integer size
+    ) {
+        Pageable pageable = PageRequest.of(0, size);
+        return ApiResponse.onSuccess(followQueryService.getCircleFollowers(member, cursorId, pageable));
     }
 }
--- src/main/java/com/coredisc/presentation/controllerdocs/CircleControllerDocs.java
+++ src/main/java/com/coredisc/presentation/controllerdocs/CircleControllerDocs.java
@@ -2,10 +2,14 @@ package com.coredisc.presentation.controllerdocs;
 
 import com.coredisc.common.apiPayload.ApiResponse;
 import com.coredisc.domain.member.Member;
+import com.coredisc.presentation.dto.follow.FollowResponseDTO;
 import com.coredisc.security.jwt.annotaion.CurrentMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Circle", description = "친한 친구 관련 API")
 public interface CircleControllerDocs {
@@ -15,4 +19,13 @@ public interface CircleControllerDocs {
 
     @Operation(summary = "친한 친구 삭제", description = "친한 친구 삭제 기능입니다.")
     ApiResponse<String> removeToCircle(@CurrentMember Member member, @PathVariable Long targetId);
+
+    @Operation(summary = "친한 친구 목록 조회", description = "친한 친구 목록 조회 기능입니다. 커서 기반 페이징입니다.")
+    @Parameters({
+            @Parameter(name = "cursorId", description = "마지막으로 조회한 followId입니다. 첫 요청 때는 null, queryString입니다."),
+            @Parameter(name = "size", description = "기본값 10")
+    })
+    ApiResponse<FollowResponseDTO.FollowerListDTO> getCircleFollowers(@CurrentMember Member member,
+                                                                      @RequestParam(required = false) Long cursorId,
+                                                                      @RequestParam(required = false) Integer size);
 }
