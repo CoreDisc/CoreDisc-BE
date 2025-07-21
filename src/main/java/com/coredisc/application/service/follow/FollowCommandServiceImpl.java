3d6eac0e680a63adb5ee3bb538171c869d8006d1
hmj6589 <hmj6589@gmail.com>
2025-07-21 19:24:56 +0900

    Feat: 친한 친구 상태 변경 service 작성

--- src/main/java/com/coredisc/application/service/follow/FollowCommandServiceImpl.java
+++ src/main/java/com/coredisc/application/service/follow/FollowCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.coredisc.application.service.follow;
 
 import com.coredisc.common.apiPayload.status.ErrorStatus;
 import com.coredisc.common.converter.FollowConverter;
+import com.coredisc.common.exception.handler.CircleHandler;
 import com.coredisc.common.exception.handler.FollowHandler;
 import com.coredisc.common.exception.handler.MemberHandler;
 import com.coredisc.domain.follow.Follow;
@@ -60,4 +61,24 @@ public class FollowCommandServiceImpl implements FollowCommandService {
         followRepository.delete(follow);
     }
 
+    @Override
+    public void updateCircleStatus(Member member, Long targetId, boolean isCircle) {
+
+        // 자기 자신을 친한친구로 설정하려는 경우
+        if (member.getId().equals(targetId)) {
+            throw new CircleHandler(ErrorStatus.SELF_CIRCLE_NOT_ALLOWED);
+        }
+
+        Member target = memberRepository.findById(targetId)
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        // 상대방이 나를 팔로우하고 있는지 확인
+        // 상대방이 나를 팔로우하고 있는 경우에만 isCircle 변경 가능
+        Follow follow = followRepository.findByFollowerAndFollowing(target, member);
+        if (follow == null) {
+            throw new FollowHandler(ErrorStatus.FOLLOWER_NOT_FOUND);
+        }
+
+        follow.updateCircle(isCircle);
+    }
 }
