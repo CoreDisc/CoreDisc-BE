16d1bc0fcaec2999b4af930a555d75eabb5a771b
jieun13 <ynb9598@gmail.com>
2025-07-23 00:31:50 +0900

    Feat: 캘린더 controller 작성

---
+++ src/main/java/com/coredisc/presentation/controller/CalendarController.java
@@ -0,0 +1,26 @@
+package com.coredisc.presentation.controller;
+
+import com.coredisc.application.service.calendar.CalendarQueryService;
+import com.coredisc.common.apiPayload.ApiResponse;
+import com.coredisc.domain.member.Member;
+import com.coredisc.presentation.controllerdocs.CalendarControllerDocs;
+import com.coredisc.presentation.dto.calendar.CalendarResponseDTO;
+import com.coredisc.security.jwt.annotaion.CurrentMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reports/calendar")
+public class CalendarController implements CalendarControllerDocs {
+
+    private final CalendarQueryService calendarQueryService;
+
+    // 캘린더 조회
+    @GetMapping
+    public ApiResponse<CalendarResponseDTO.CalendarDTO> getCalendar(int year, int month, @CurrentMember Member member) {
+        return ApiResponse.onSuccess(calendarQueryService.getCalendar(year, month, member));
+    }
+}
