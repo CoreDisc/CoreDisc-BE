3a90c6eb2e2b0976f8acc5f55a26d2323950d093
dokyung-kang <kangdk025@gmail.com>
2025-07-24 12:35:05 +0900

    refactor: 질문 카테고리 조회 API

--- src/main/java/com/coredisc/infrastructure/repository/category/qeurydsl/QueryCategoryRepositoryImpl.java
+++ src/main/java/com/coredisc/infrastructure/repository/category/qeurydsl/QueryCategoryRepositoryImpl.java
@@ -2,12 +2,8 @@ package com.coredisc.infrastructure.repository.category.qeurydsl;
 
 import com.coredisc.domain.category.QCategory;
 import com.coredisc.domain.mapping.questionCategory.QQuestionCategory;
-import com.coredisc.domain.officialQuestion.QOfficialQuestion;
-import com.coredisc.domain.personalQuestion.QPersonalQuestion;
 import com.coredisc.presentation.dto.category.CategoryResponseDTO;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.CaseBuilder;
-import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -24,31 +20,17 @@ public class QueryCategoryRepositoryImpl implements QueryCategoryRepository {
     public List<CategoryResponseDTO.CategoryDTO> findCategoryList() {
         QCategory qCategory = QCategory.category;
         QQuestionCategory qQuestionCategory = QQuestionCategory.questionCategory;
-        QOfficialQuestion qOfficialQuestion = QOfficialQuestion.officialQuestion;
-        QPersonalQuestion qPersonalQuestion = QPersonalQuestion.personalQuestion;
-
-        NumberExpression<Long> conditionalCount = new CaseBuilder()
-                .when(
-                        qOfficialQuestion.isShared.eq(false)
-                                .or(qQuestionCategory.personalQuestion.isNotNull())
-                )
-                .then(1L)
-                .otherwise(0L)
-                .sum();
 
         return jpaQueryFactory
                 .select(Projections.constructor(
                         CategoryResponseDTO.CategoryDTO.class,
                         qCategory.id,
                         qCategory.name,
-                        conditionalCount
+                        qQuestionCategory.id.count()
                 ))
                 .from(qCategory)
                 .leftJoin(qQuestionCategory).on(qQuestionCategory.category.eq(qCategory))
-                .leftJoin(qQuestionCategory.officialQuestion, qOfficialQuestion)
-                .leftJoin(qQuestionCategory.personalQuestion, qPersonalQuestion)
                 .groupBy(qCategory.id)
                 .fetch();
-
     }
 }
