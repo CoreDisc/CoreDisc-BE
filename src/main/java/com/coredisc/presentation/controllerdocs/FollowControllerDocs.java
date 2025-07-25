8cc0f9209865e97e3881802c72cd225b3a852ac5
hmj6589 <hmj6589@gmail.com>
2025-07-26 02:35:21 +0900

    Refactor: 언팔로우 API 반환 타입 String으로 수정

--- src/main/java/com/coredisc/presentation/controllerdocs/FollowControllerDocs.java
+++ src/main/java/com/coredisc/presentation/controllerdocs/FollowControllerDocs.java
@@ -18,7 +18,7 @@ public interface FollowControllerDocs {
     ApiResponse<FollowResponseDTO.FollowResultDTO> follow(@CurrentMember Member member, @PathVariable Long targetId);
 
     @Operation(summary = "팔로우 취소", description = "팔로우 취소(팔로윙 삭제) 기능입니다.")
-    ApiResponse<?> unfollow(@CurrentMember Member member, @PathVariable Long targetId);
+    ApiResponse<String> unfollow(@CurrentMember Member member, @PathVariable Long targetId);
 
     //TODO: description 수정
     @Operation(summary = "팔로워 목록 조회", description = "팔로워 목록 조회 기능입니다. 커서 기반 페이징입니다.")
