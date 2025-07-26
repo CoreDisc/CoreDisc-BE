9fda1d490c3d23a19c80fe4d9b16a044ca31e657
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-26 13:29:41 +0900

    Feat: 게시글 개수 count 관련 repository 작성

--- src/main/java/com/coredisc/infrastructure/repository/post/JpaPostRepository.java
+++ src/main/java/com/coredisc/infrastructure/repository/post/JpaPostRepository.java
@@ -1,15 +1,18 @@
 package com.coredisc.infrastructure.repository.post;
 
 
-import com.coredisc.domain.member.QMember;
+import com.coredisc.domain.common.enums.PostStatus;
+import com.coredisc.domain.common.enums.PublicityType;
+import com.coredisc.domain.member.Member;
 import com.coredisc.domain.post.Post;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface JpaPostRepository extends JpaRepository<Post, Long> {
 
+    long countByMemberAndStatus(Member member, PostStatus status);
+    long countByMemberAndStatusAndPublicityIn(Member member, PostStatus status, List<PublicityType> publicityTypes);
 }
 
 
