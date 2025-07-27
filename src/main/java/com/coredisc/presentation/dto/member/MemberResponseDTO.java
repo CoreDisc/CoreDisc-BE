bfdcd2f70af96b87702307b49985869f146a907d
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-07-27 10:17:54 +0900

    Refactor: dto, converter 하나로 통일

--- src/main/java/com/coredisc/presentation/dto/member/MemberResponseDTO.java
+++ src/main/java/com/coredisc/presentation/dto/member/MemberResponseDTO.java
@@ -1,6 +1,5 @@
 package com.coredisc.presentation.dto.member;
 
-import com.coredisc.domain.common.enums.PublicityType;
 import com.coredisc.presentation.dto.profileImg.ProfileImgResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -63,22 +62,7 @@ public class MemberResponseDTO {
 
         private Long postId;
 
-        private PublicityType publicityType;
-
-        private PostImageThumbnailDTO postImageThumbnailDTO; // 4개의 답변 중 첫 번째 답변이 이미지 답변안 경우
-
-        private PostTextThumbnailDTO postTextThumbnailDTO; // 4개의 답변 중 첫 번째 답변이 텍스트 답변인 경우
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class UserHomePostDTO {
-
-        private Long postId;
-
-        private PostImageThumbnailDTO postImageThumbnailDTO; // 4개의 답변 중 첫 번째 답변이 이미지 답변안 경우
+        private PostImageThumbnailDTO postImageThumbnailDTO; // 4개의 답변 중 첫 번째 답변이 이미지 답변인 경우
 
         private PostTextThumbnailDTO postTextThumbnailDTO; // 4개의 답변 중 첫 번째 답변이 텍스트 답변인 경우
     }
@@ -98,12 +82,7 @@ public class MemberResponseDTO {
     @NoArgsConstructor
     public static class PostTextThumbnailDTO {
 
-        private String weekday;
-
-        private String createdAt;
-
-        // TODO: 답변의 질문 카테고리
-//        private List<String> categories;
+        private String content;
     }
 
 }
