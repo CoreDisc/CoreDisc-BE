c2e2b40aabc06d53e5682efce41807899e6704ff
jieun13 <ynb9598@gmail.com>
2025-07-29 02:40:01 +0900

    Feat: 서비스 로직 구현

--- src/main/java/com/coredisc/application/service/reportStat/ReportStatQueryService.java
+++ src/main/java/com/coredisc/application/service/reportStat/ReportStatQueryService.java
@@ -1,6 +1,9 @@
 package com.coredisc.application.service.reportStat;
 
-import java.time.LocalDate;
+import com.coredisc.domain.member.Member;
+import com.coredisc.domain.post.Post;
+
+import java.util.List;
 
 public interface ReportStatQueryService {
 
@@ -15,4 +18,7 @@ public interface ReportStatQueryService {
 
     // 기간별 게시글의 daily_ 항목 최다 답변 조회
     ReportRawData.DailyOptionRawData getMostSelectedDaily(int year, int month, Long memberId);
+
+    // 기간별 daily_detail 항목 답변 목록 조회
+    List<Post> getDailyDetails(int year, int month, Member member);
 }
