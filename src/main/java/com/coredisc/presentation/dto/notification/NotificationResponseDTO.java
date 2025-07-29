3d266a090de45ef6fc248eda5a583a15f73f7084
hmj6589 <hmj6589@gmail.com>
2025-07-30 02:18:13 +0900

    Feat: notification dto 작성

---
+++ src/main/java/com/coredisc/presentation/dto/notification/NotificationResponseDTO.java
@@ -0,0 +1,34 @@
+package com.coredisc.presentation.dto.notification;
+
+import com.coredisc.domain.common.enums.NotificationType;
+import com.coredisc.presentation.dto.profileImg.ProfileImgResponseDTO;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+/*
+    - 알림 id
+    - 알림 타입
+    - 알림 내용
+    - 타겟id(팔로우, 게시글 등의 id)
+    - 보낸 유저 id
+    - 보낸 유저의 nickname
+    - 보낸 유저의 프로필 이미지
+    - 알림 읽음 여부
+    - 알림 생성 시간
+    - 타임 스탬프
+*/
+
+@Builder
+public record NotificationResponseDTO (
+    Long notificationId,
+    NotificationType type,
+    String content,
+    Long targetId,
+    Long senderId,
+    String senderNickname,
+    ProfileImgResponseDTO.ProfileImgDTO profileImgDTO,
+    boolean isRead,
+    LocalDateTime createdAt,
+    String timeStamp
+){}
