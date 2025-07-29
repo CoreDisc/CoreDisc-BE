161d1b35eb7afb10dfa141353725c081b3cd434f
hmj6589 <hmj6589@gmail.com>
2025-07-30 02:25:24 +0900

    Refactor: 사용하지 않는 import문 삭제

--- src/main/java/com/coredisc/presentation/controllerdocs/NotificationControllerDocs.java
+++ src/main/java/com/coredisc/presentation/controllerdocs/NotificationControllerDocs.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
