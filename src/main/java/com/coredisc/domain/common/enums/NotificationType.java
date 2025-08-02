9d80a373e639b4ecdc60f2a93bdcf7bf9c762a58
hmj6589 <hmj6589@gmail.com>
2025-08-02 21:43:29 +0900

    Refactor: 댓글, 대댓글 알림 타입 추가

--- src/main/java/com/coredisc/domain/common/enums/NotificationType.java
+++ src/main/java/com/coredisc/domain/common/enums/NotificationType.java
@@ -5,5 +5,7 @@ public enum NotificationType {
     UNANSWERED_QUESTION, //질문 답변 마무리해줘 리마인더
     MONTHLY_REPORT, //한 달 리포트
     FOLLOW, //팔로우 알림 (누가 날 팔로우 했어요)
-    SHARED_SAVED //공유 질문 저장 알림 (내가 작성한 공유 질문 누가 저장했어요)
+    SHARED_SAVED, //공유 질문 저장 알림 (내가 작성한 공유 질문 누가 저장했어요)
+    COMMENT, // 댓글 알림
+    COMMENT_REPLY // 대댓글 알림
 }
