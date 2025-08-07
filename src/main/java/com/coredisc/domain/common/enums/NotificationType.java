00a909f6bc31df0f758b39de21db5173ce0d5cca
hmj6589 <hmj6589@gmail.com>
2025-08-07 17:24:10 +0900

    Refactor: 좋아요, 임시저장 알림 타입 enum 추가

--- src/main/java/com/coredisc/domain/common/enums/NotificationType.java
+++ src/main/java/com/coredisc/domain/common/enums/NotificationType.java
@@ -7,5 +7,7 @@ public enum NotificationType {
     FOLLOW, //팔로우 알림 (누가 날 팔로우 했어요)
     SHARED_SAVED, //공유 질문 저장 알림 (내가 작성한 공유 질문 누가 저장했어요)
     COMMENT, // 댓글 알림
-    COMMENT_REPLY // 대댓글 알림
+    COMMENT_REPLY, // 대댓글 알림
+    LIKE, // 좋아요 알림
+    TEMP_POSTS // 임시저장 알림
 }
