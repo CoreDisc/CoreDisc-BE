13c2deb2eddb2bce38a041e7aad811376d633baa
hmj6589 <hmj6589@gmail.com>
2025-08-09 03:34:08 +0900

    Refactor: 트랜잭션 어노테이션 추가

--- src/main/java/com/coredisc/application/schedule/NotificationScheduler.java
+++ src/main/java/com/coredisc/application/schedule/NotificationScheduler.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,6 +23,7 @@ import java.util.Set;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class NotificationScheduler {
 
     private final PostRepository postRepository;
