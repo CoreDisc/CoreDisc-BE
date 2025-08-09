5598aad21682a4a1c935d3d7daf61e4ccbbf1acf
jieun13 <ynb9598@gmail.com>
2025-08-09 19:30:18 +0900

    fix: 디스크 커버 이미지 업데이트 예외 처리 추가

--- src/main/java/com/coredisc/application/service/disc/DiscCommandServiceImpl.java
+++ src/main/java/com/coredisc/application/service/disc/DiscCommandServiceImpl.java
@@ -3,6 +3,7 @@ package com.coredisc.application.service.disc;
 import com.coredisc.common.apiPayload.status.ErrorStatus;
 import com.coredisc.common.converter.DiscConverter;
 import com.coredisc.common.exception.handler.DiscHandler;
+import com.coredisc.common.exception.handler.ProfileImgHandler;
 import com.coredisc.domain.common.enums.DiscCoverColor;
 import com.coredisc.domain.disc.Disc;
 import com.coredisc.domain.disc.DiscRepository;
@@ -50,6 +51,8 @@ public class DiscCommandServiceImpl implements DiscCommandService {
 
         } catch (IOException e) {
             throw new DiscHandler(ErrorStatus.FILE_UPLOAD_FAILED);
+        } catch (Exception e) {
+        throw new DiscHandler(ErrorStatus.UNKNOWN_ERROR);
         }
 
         return discRepository.save(disc);
