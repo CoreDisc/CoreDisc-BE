c3512d72af590925d23abbf047e05053b62b5686
hmj6589 <hmj6589@gmail.com>
2025-08-10 04:07:48 +0900

    Feat: 리마인더 알림 설정 변경 service 작성

--- src/main/java/com/coredisc/application/service/notificationReminderSetting/NotificationReminderSettingCommandServiceImpl.java
+++ src/main/java/com/coredisc/application/service/notificationReminderSetting/NotificationReminderSettingCommandServiceImpl.java
@@ -1,8 +1,13 @@
 package com.coredisc.application.service.notificationReminderSetting;
 
+import com.coredisc.common.apiPayload.status.ErrorStatus;
 import com.coredisc.common.converter.NotificationReminderSettingConverter;
+import com.coredisc.common.exception.handler.NotificationReminderSettingHandler;
+import com.coredisc.domain.mapping.notificationReminderSetting.NotificationReminderSetting;
 import com.coredisc.domain.mapping.notificationReminderSetting.NotificationReminderSettingRepository;
 import com.coredisc.domain.member.Member;
+import com.coredisc.presentation.dto.notificationReminderSetting.NotificationReminderSettingRequestDTO;
+import com.coredisc.presentation.dto.notificationReminderSetting.NotificationReminderSettingResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -28,5 +33,52 @@ public class NotificationReminderSettingCommandServiceImpl implements Notificati
                         .orElseThrow();
             }
         });
+    }
+
+    @Override
+    public NotificationReminderSettingResponseDTO updateNotificationReminderSetting(
+            Member member,
+            NotificationReminderSettingRequestDTO.NotificationReminderSettingUpdateDTO request
+    ) {
+
+        NotificationReminderSetting setting = notificationReminderSettingRepository.findByMember(member).orElseThrow();
+
+        // 데일리 리마인더 알림 on/off 변경
+        if (request.getDailyReminderEnabled() != null) {
+            setting.updateDailyReminderEnabled(request.getDailyReminderEnabled());
+        }
+
+        // 데일리 리마인더 알림 시간 변경
+        if (request.getDailyReminderHour() != null || request.getDailyReminderMinute() != null) {
+            int h = (request.getDailyReminderHour()   != null) ? request.getDailyReminderHour()   : setting.getDailyReminderTime().getHour();
+            int m = (request.getDailyReminderMinute() != null) ? request.getDailyReminderMinute() : setting.getDailyReminderTime().getMinute();
+            setting.changeDailyReminderTime(h, m);
+        }
+
+        // unanswered 알림 on/off 변경
+        if (request.getUnansweredReminderEnabled() != null) {
+            setting.updateUnansweredReminderEnabled(request.getUnansweredReminderEnabled());
+        }
+
+        // unanswered 알림 시간 변경
+        if (request.getUnansweredReminderHour() != null || request.getUnansweredReminderMinute() != null) {
+            int h = (request.getUnansweredReminderHour()   != null) ? request.getUnansweredReminderHour()   : setting.getUnansweredReminderTime().getHour();
+            int m = (request.getUnansweredReminderMinute() != null) ? request.getUnansweredReminderMinute() : setting.getUnansweredReminderTime().getMinute();
+            setting.changeUnansweredReminderTime(h, m);
+        }
+
+        // 근데 데일리 리마인더가 off라면 unanwered도 off되도록
+        if (!setting.isDailyReminderEnabled()) {
+            setting.updateUnansweredReminderEnabled(false);
+        }
+
+        // 둘 다 true 일 때, unanswered 알림 시간 > 데일리 알림 시간
+        if (setting.isDailyReminderEnabled() && setting.isUnansweredReminderEnabled()) {
+            if (!setting.getDailyReminderTime().isAfter(setting.getDailyReminderTime())) {
+                throw new NotificationReminderSettingHandler(ErrorStatus.INVALID_REMINDER_TIME_ORDER);
+            }
+        }
+
+        return NotificationReminderSettingConverter.toResponseDTO(setting);
     }
 }
