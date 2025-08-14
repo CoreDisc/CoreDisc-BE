8274bc6bc3bf4db6b2a18d0aaedda6d55536f2b3
jieun13 <ynb9598@gmail.com>
2025-08-14 15:19:39 +0900

    refactor: 디스크 목록 한 번에 조회하도록 수정

--- src/main/java/com/coredisc/application/service/disc/DiscQueryServiceImpl.java
+++ src/main/java/com/coredisc/application/service/disc/DiscQueryServiceImpl.java
@@ -7,9 +7,9 @@ import com.coredisc.domain.disc.Disc;
 import com.coredisc.domain.disc.DiscRepository;
 import com.coredisc.domain.member.Member;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,28 +19,24 @@ public class DiscQueryServiceImpl implements DiscQueryService {
     private final DiscCreationHelper discCreationHelper;
 
     @Override
-    public Page<Disc> getMyDiscList(Member member, Pageable pageable) {
-        Page<Disc> discPage = discRepository.findByMember(member, pageable);
+    public List<Disc> getMyDiscList(Member member) {
+        List<Disc> discList = discRepository.findAllByMemberOrderByYearDescMonthDesc(member);
 
         // 디스크가 하나도 없을 때 (새로 회원가입한 경우 매월 1일에 자동생성되는 디스크가 없음)
-        if (discPage.isEmpty()) {
-            if (pageable.getPageNumber() > 0) {
-                throw new DiscHandler(ErrorStatus.PAGE_OUT_OF_BOUNDS);
-            }
-
+        if (discList.isEmpty()) {
             // 이번 달 디스크 생성
             discCreationHelper.ensureCurrentMonthDisc(member);
 
             // 다시 조회
-            discPage = discRepository.findByMember(member, pageable);
+            discList = discRepository.findAllByMemberOrderByYearDescMonthDesc(member);
 
             // 그래도 없으면 예외 처리
-            if (discPage.isEmpty()) {
+            if (discList.isEmpty()) {
                 throw new DiscHandler(ErrorStatus.DISC_NOT_FOUND);
             }
         }
 
-        return discPage;
+        return discList;
     }
 
     @Override
--- src/main/java/com/coredisc/infrastructure/repository/disc/JpaDiscRepository.java
+++ src/main/java/com/coredisc/infrastructure/repository/disc/JpaDiscRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaDiscRepository extends JpaRepository<Disc, Long> {
     Long countByMember(Member member);
     Page<Disc> findByMember(Member member, Pageable pageable);
+    List<Disc> findAllByMemberOrderByYearDescMonthDesc(Member member);
     Optional<Disc> findByIdAndMember(Long id, Member member);
     boolean existsByMemberAndYearAndMonth(Member member, int year, int month);
 }
